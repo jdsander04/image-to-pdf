4aec4fbfecf60f809af337bae98239906ffc1354
jdsander04 <100306874+jdsander04@users.noreply.github.com>
2023-07-26 12:15:30 -0400

    Update itop.py

--- itop.py
+++ itop.py
@@ -1,4 +1,6 @@
 import tkinter as itop
+from tkinter import filedialog
+from tkinter import ttk
 import os
 import img2pdf
 from PIL import Image
@@ -6,6 +8,9 @@ import re
 from sys import stdout as terminal
 from time import sleep
 import math
+
+parentFolder = "Select the parent folder"
+output = "Select the output folder"
 
 def convert_images_to_pdf(folder_path, output_file):
     image_files = [file for file in os.listdir(folder_path) if file.lower().endswith(('.jpg', '.png'))]
@@ -40,9 +45,9 @@ def process_child_folders(parent_folder, pdf_placement):
 
         output_file = os.path.join(pdf_placement, pdf_name)
 
-        Progress = math.floor((i/num_folders)*20)
-        bar = Progress*chr(9619)+'_'*(20-Progress)
-        terminal.write("\r["+bar+"]  " + str(i) + "/" + str(num_folders))
+        #Progress = math.floor((i/num_folders)*20)
+        #bar = Progress*chr(9619)+'_'*(20-Progress)
+        #terminal.write("\r["+bar+"]  " + str(i) + "/" + str(num_folders))
 
         try:
             convert_images_to_pdf(child_folder, output_file)
@@ -51,20 +56,24 @@ def process_child_folders(parent_folder, pdf_placement):
 
     print("\nProcessing completed!")
 
-    input("Press Enter to Continue")
+def start_button_click():
+    global parentFolder
+    global output
 
-
-
-def button_click():
-    parent = parent_folder.get()
-    output = output_folder.get()
-    if not os.path.isdir(parent):
+    if not os.path.isdir(parentFolder):
         print("Invalid parent folder path.")
     elif not os.path.isdir(output):
         print("invalid pdf output folder.")
     else:
-        process_child_folders(parent, output)
+        process_child_folders(parentFolder, output)
+
+def selectParentFolder():
+    global parentFolder
+    parentFolder = filedialog.askdirectory(title="Select the parent folder folder")
     
+def selectOuputFolder():
+    global output
+    output = filedialog.askdirectory(title="Select the output folder")
 
 root = itop.Tk()
 
@@ -73,20 +82,13 @@ root.title("iTop (hot girls)")
 label = itop.Label(root, text="Welcome to the image to pdf python script!")
 label.pack()
 
-input_label = itop.Label(root, text="Parent Directory")
-input_label.pack()
+parentFolderButton = itop.Button(root, text=parentFolder, command=selectParentFolder)
+parentFolderButton.pack()
 
-parent_folder = itop.Entry(root)
-parent_folder.pack()
+outputFolderButton = itop.Button(root, text=output, command=selectOuputFolder)
+outputFolderButton.pack()
 
-output_label = itop.Label(root, text="Output Directory")
-output_label.pack()
-
-output_folder = itop.Entry(root)
-output_folder.pack()
-
-button = itop.Button(root, text="Images to PDF!", command=button_click)
+button = itop.Button(root, text="Images to PDF!", command=start_button_click)
 button.pack()
-
 
 root.mainloop()
